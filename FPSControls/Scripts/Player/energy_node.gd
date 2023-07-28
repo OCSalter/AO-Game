39d0f1a3a8df49ac38d61f8dc71e5b8fbbb6995e
OCSalter <owensalter@outlook.com>
2023-07-28 15:29:40 -0400

    Add energy burn attempt and response

--- FPSControls/Scripts/Player/energy_node.gd
+++ FPSControls/Scripts/Player/energy_node.gd
@@ -2,28 +2,31 @@ extends Node3D
 
 @onready var empty_timer = $EmptyTimer
 
-var meter: float
+var max_meter: float
 var current_meter: float
 var recharge_rate: float
 
 
-func setup(wait_time: float = 0.5, starting_meter: float = 100, starting_recharge_rate: float = 50):
+func setup(wait_time: float = 0.5, starting_meter: float = 100, starting_recharge_rate: float = 5):
 	empty_timer.wait_time = wait_time
-	meter = starting_meter
-	current_meter = meter
+	max_meter = starting_meter
+	current_meter = max_meter
 	recharge_rate = starting_recharge_rate
 	
 func _physics_process(delta):
 	if can_recharge():
 		current_meter += recharge_rate * delta
 
-func burn_meter(amount: float) -> bool:
+func try_then_burn(amount: float) -> bool:
 	if current_meter <= 0:
 		return false
+	burn_meter(amount)
+	return true
+	
+func burn_meter(amount: float):
 	current_meter = current_meter - amount if current_meter > amount else 0
 	if current_meter == 0:
 		empty_timer.start()
-	return true
 	
 func can_recharge() -> bool:
-	return empty_timer.is_stopped()
+	return max_meter > current_meter and empty_timer.is_stopped()
